6e4617d11a4fed11dffb47e368c32ba9c3bf5e4d
muhammad1339 <muhammadahmed1339@gmail.com>
2019-05-31 00:10:58 +0200

    Started-Project

--- app/src/main/java/com/proprog/e_commerce/MainActivity.kt
+++ app/src/main/java/com/proprog/e_commerce/MainActivity.kt
@@ -2,11 +2,21 @@ package com.proprog.e_commerce
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.support.v7.app.ActionBarDrawerToggle
+import kotlinx.android.synthetic.main.activity_main.*
+import kotlinx.android.synthetic.main.app_bar_layout.*
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.main_menu_layout)
+//        setSupportActionBar(toolbar_main)
+//        supportActionBar?.title = "E-Commerce"
+//        val toggle = ActionBarDrawerToggle(
+//            this, drawer_layout, toolbar_main, R.string.open, R.string.close
+//        )
+//        drawer_layout.addDrawerListener(toggle)
+//        toggle.syncState()
     }
 }
